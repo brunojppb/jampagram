0b8c72c2ba844c8bb7532eba0a3388b474d2af83
Bruno Paulino <brunojppb@gmail.com>
2016-02-09 21:36:08 -0300

    Setup Devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
+  before_save :skip_confirmation!
+end
